d4e9e0febf016fa8cd7fe2cb4cab3a67e6fe9231
wenpeng <676395159@qq.com>
2017-11-28 22:59:36 +0800

    添加空项目

---
+++ MyChild/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyChild.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
